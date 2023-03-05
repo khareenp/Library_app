6ac07664f3c24705f1301d5e0f60ffb0ccf55f0d
khareenp <khareenp1@gmail.com>
2023-03-05 12:55:11 -0500

    api config

--- 02-backend/spring-boot-library/src/main/java/com/luve2code/springbootlibrary/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-library/src/main/java/com/luve2code/springbootlibrary/config/MyDataRestConfig.java
@@ -1,2 +1,37 @@
-package com.luve2code.springbootlibrary.config;public class MyDataRestConfig {
+package com.luve2code.springbootlibrary.config;
+
+import com.luve2code.springbootlibrary.entity.Book;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+
+/*Spring rest jpa automatically creates all CRUD api endpoints and exposes them
+* Inorder to make the application read only we must disable the update,delete,post methods (APIs) endpoints
+* unless this is required by the application
+* */
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+    private String theAllowedOrigins = "http://localhost:3000";
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+        HttpMethod[] theUnsuppotedActions = {HttpMethod.POST,HttpMethod.PATCH,HttpMethod.DELETE,HttpMethod.PUT};
+
+        config.exposeIdsFor(Book.class);
+
+        disableHttpMethods(Book.class,config,theUnsuppotedActions);
+
+        /*Configure CORS Mapping*/
+        cors.addMapping(config.getBasePath() + "/**").allowedOrigins(theAllowedOrigins);
+
+
+    }
+
+    private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions){
+        config.getExposureConfiguration().forDomainType(theClass).withAssociationExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))).withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+    }
 }
